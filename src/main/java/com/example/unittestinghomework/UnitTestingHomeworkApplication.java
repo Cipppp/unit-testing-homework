72fcb05b124ddb93449b2212e73eec9814b45b93
Cipppp <pirvuciprian37@gmail.com>
2022-04-19 13:33:08 +0300

    Unit testing homework added

---
+++ src/main/java/com/example/unittestinghomework/UnitTestingHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittestinghomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unittestinghomework/UnitTestingHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittestinghomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
